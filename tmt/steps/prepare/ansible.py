422176ec38dba61ec21cb6aa0d5df752160aacc7
Filip Vágner <fvagner@redhat.com>
2025-02-18 15:40:58 +0100

    Save remote ansible playbooks to correct directory

--- tmt/steps/prepare/ansible.py
+++ tmt/steps/prepare/ansible.py
@@ -185,7 +185,8 @@ class PrepareAnsible(tmt.steps.prepare.PreparePlugin[PrepareAnsibleData]):
             lowercased_playbook = _playbook.lower()
 
             def normalize_remote_playbook(raw_playbook: str) -> Path:
-                root_path = self.step.plan.anchor_path
+                assert self.step.workdir is not None  # narrow type
+                root_path = self.step.workdir
 
                 try:
                     with retry_session(
@@ -224,8 +225,11 @@ class PrepareAnsible(tmt.steps.prepare.PreparePlugin[PrepareAnsibleData]):
                 return AnsibleCollectionPlaybook(raw_playbook)
 
             playbook: AnsibleApplicable
+            playbook_root = self.step.plan.anchor_path
 
             if lowercased_playbook.startswith(('http://', 'https://')):
+                assert self.step.workdir is not None  # narrow type
+                playbook_root = self.step.workdir
                 playbook = normalize_remote_playbook(lowercased_playbook)
 
             elif lowercased_playbook.startswith('file://'):
@@ -239,7 +243,7 @@ class PrepareAnsible(tmt.steps.prepare.PreparePlugin[PrepareAnsibleData]):
 
             guest.ansible(
                 playbook,
-                playbook_root=self.step.plan.anchor_path,
+                playbook_root=playbook_root,
                 extra_args=self.data.extra_args,
             )
 
